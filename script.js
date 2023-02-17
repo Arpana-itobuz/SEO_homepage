53ec78143cb96e353cde55b13828ffc8b88ccf37
Arpana <arpana@itobuz.com>
2023-02-17 18:27:42 +0530

    updated 2

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-const ourPlans = document.querySelectorAll('.websiteCostPlans')
-const navBarItems = document.querySelectorAll('a')
+const ourPlans = document.querySelectorAll(".websiteCostPlans");
+const Plans = document.querySelectorAll(".cost");
+// const navBarItems = document.querySelectorAll("a");
 console.log(ourPlans);
 
-ourPlans.forEach((item)=>{
+ourPlans.forEach((item) => {
+  item.addEventListener("mouseover", function () {
+    console.log("hi");
+    item.style.transform = "scale(1.03)";
+    // Plans.style.color="white"
+    item.style.color ="white";
+    item.style.transition = "transform 2s";
+  });
+
+  item.addEventListener("mouseout", function () {
+    console.log("hi");
+    item.style.color ="black";
+    item.style.transform = "scale(1)";
+    item.style.transition = "transform 1s";
+  });
+});
+
+Plans.forEach((item) => {
     item.addEventListener("mouseover",function(){
-        console.log('hi');
-        item.style.transform="scale(1.2)";
-        item.style.transition="transform 2s"
+        item.style.color="white"
     })
 
     item.addEventListener("mouseout",function(){
-        console.log('hi');
-        item.style.transform="scale(1)";
-        item.style.transition="transform 1s"
+        item.style.color="#6f77f1"
     })
-})
+});
 
-navBarItems.forEach((item)=>{
-    item.addEventListener("mouseover",function(){
-        // item.style.color="red"
-        item.style.borderBottom="1px groove "
-        // item.style.animation
-        
-    })
 
-    item.addEventListener("mouseout",function(){
-        item.style.borderBottom="none"
-        // item.style.color="black"
-    })
-})
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    autoplay: {
+        delay:2500,
+        disableOnInteraction:false,
+
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    
+      clickable: true,
+    //   hideOnClick:true,
+    },
+  });
