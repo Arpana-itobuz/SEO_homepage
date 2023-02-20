64761c6c216b7da048ffbf019ac30f160ed0458a
Arpana <arpana@itobuz.com>
2023-02-20 18:51:16 +0530

    new updatation

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-const ourPlans = document.querySelectorAll(".websiteCostPlans");
+const menu = document.getElementById("menu");
+const nav_list = document.getElementById("nav_list");
+menu.addEventListener("click", () => {
+  console.log('hi');
+  if (nav_list.classList.contains("hide")) {
+    nav_list.classList.remove("hide");
+    nav_list.classList.add("show");
+  } else {
+    nav_list.classList.add("hide");
+    nav_list.classList.remove("show");
+  }
+});
+
+const ourPlans = document.querySelectorAll(".website-cost-plans");
 const Plans = document.querySelectorAll(".cost");
 // const navBarItems = document.querySelectorAll("a");
 console.log(ourPlans);
@@ -7,7 +20,8 @@ ourPlans.forEach((item) => {
   item.addEventListener("mouseover", function () {
     console.log("hi");
     item.style.transform = "scale(1.03)";
-    // Plans.style.color="white"
+    item.firstElementChild.style.color="white"
+    item.lastElementChild.style.background="yellow"
     item.style.color ="white";
     item.style.transition = "transform 2s";
   });
@@ -16,20 +30,11 @@ ourPlans.forEach((item) => {
     console.log("hi");
     item.style.color ="black";
     item.style.transform = "scale(1)";
+    item.firstElementChild.style.color="#6f77f1"
+    item.lastElementChild.style.background="none"
     item.style.transition = "transform 1s";
   });
 });
-
-Plans.forEach((item) => {
-    item.addEventListener("mouseover",function(){
-        item.style.color="white"
-    })
-
-    item.addEventListener("mouseout",function(){
-        item.style.color="#6f77f1"
-    })
-});
-
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
