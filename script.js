e61b592dd1dfb60931fa07206a2a23d6d82e6fe3
Arpana <arpana@itobuz.com>
2023-02-21 13:26:47 +0530

    one more final update

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menu = document.getElementById("menu");
 const nav_list = document.getElementById("nav_list");
+const searchButton = document.querySelector(".search-button")
+const searchBar = document.querySelector(".search-bar")
 menu.addEventListener("click", () => {
   console.log('hi');
   if (nav_list.classList.contains("hide")) {
@@ -28,3 +30,14 @@ var swiper = new Swiper(".mySwiper", {
     //   hideOnClick:true,
     },
   });
+
+  searchButton.addEventListener("click", ()=>{
+    if (searchBar.classList.contains("show")){
+      searchBar.classList.remove("show")
+      searchBar.classList.add("hide")
+    }
+    else {
+      searchBar.classList.remove("hide")
+      searchBar.classList.add("show")
+    }
+  })
